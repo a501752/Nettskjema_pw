1ea77470faedc974f16c301ae20491b1597f70d1
Espen Smith <espen.smith@fremtind.no>
2025-03-28 14:22:36 +0100

    NSK-731 PlayWright test skademelding-autolease

---
+++ tests/skademelding-autolease.spec.ts
@@ -0,0 +1,383 @@
+import { test, expect } from '@playwright/test';
+      
+/** Hva har skjedd - (kollisjon | dyr | tyveri | annet) */
+
+/** Kollisjon med annet kjøretøy */
+/** Påkjøring av dyr */
+/** Tyveri/innbrudd */
+/** Andre skader kjøretøy */
+
+
+test('Skademeldingskjema etter kollisjon', async ({ page }) => {
+  
+    await page.goto('https://nettskjema.test.fremtind.no/skjema/katalog/skademelding-autolease');
+
+    await expect(page).toHaveTitle('Skademelding Autolease');
+
+    const headerInformasjon = page.locator('h2', { hasText: 'Informasjon' });
+    await expect(headerInformasjon).toBeVisible();
+
+    await page.locator('#innmelderFnr').click();
+    await page.locator('#innmelderFnr').fill('09058435551');
+    await page.locator('#innmelderFnr').press('Tab');
+    await page.locator('#innmelderNavn').click();
+    await page.locator('#innmelderNavn').fill('Anders And');
+    await page.locator('#innmelderNavn').press('Tab');
+    await page.locator('#bedriftNavn').click();
+    await page.locator('#bedriftNavn').fill('Selskapet AS');
+    await page.locator('#bedriftNavn').press('Tab');
+    await page.locator('#bedriftOrgnr').click();
+    await page.locator('#bedriftOrgnr').fill('733801433');
+    await page.locator('#bedriftOrgnr').press('Tab');
+    await page.locator('#regNr').click();
+    await page.locator('#regNr').fill('AB12345');
+    await page.locator('#regNr').press('Tab');
+    await page.locator('#kontaktEpost').click();
+    await page.locator('#kontaktEpost').fill('ole@olsen.no');
+    await page.locator('#kontaktEpost').press('Tab');
+    await page.locator('#kontaktTelefon').click();
+    await page.locator('#kontaktTelefon').fill('22334455');
+    await page.locator('#kontaktTelefon').press('Tab');
+    /** */
+    await page.getByTestId('jkl-datepicker__trigger').click();
+    await page.getByTestId('jkl-datepicker__input').fill('20.03.2025');
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+
+    
+    /** ----------------------------  */ 
+
+    const headerHvaHarSkjedd = page.locator('h2', { hasText: 'Hva har skjedd?' });
+    await expect(headerHvaHarSkjedd).toBeVisible();
+
+    await page.getByText('Angi årsak');
+    await page.locator('label').filter({ hasText: 'Kollisjon med annet kjøretøy' }).locator('span').first().click();
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+
+    /** ----------------------------  */
+    const headerKollisjonAnnetKjoretoy = page.locator('h2', { hasText: 'Kollisjon med annet kjøretøy' });
+    await expect(headerKollisjonAnnetKjoretoy).toBeVisible();
+
+    await page.locator('#kollisjonHvor').click();
+    await page.locator('#kollisjonHvor').fill('På vei til byen');
+    await page.locator('#kollisjonHvor').press('Tab');
+    await page.locator('#kollisjonSjaforNavn').click();
+    await page.locator('#kollisjonSjaforNavn').fill('Pelle Pettersen');
+    await page.locator('#kollisjonSjaforNavn').press('Tab');
+    await page.locator('#kollisjonSjaforAlder').click();
+    await page.locator('#kollisjonSjaforAlder').fill('33');
+    await page.locator('#kollisjonSjaforAlder').press('Tab');
+    await page.locator('#kollisjonHvordan').click();
+    await page.locator('#kollisjonHvordan').fill('Helt uventet sto det en bil i veikanten');
+    await page.locator('#kollisjonHvordan').press('Tab');
+    await page.getByText('Hva slags dekk var det på kjøretøyet?');
+    await page.locator('label').filter({ hasText: 'Sommerdekk' }).locator('span').first().click();
+    await page.locator('#kollisjonMotpartRegnr').click();
+    await page.locator('#kollisjonMotpartRegnr').fill('ZN54321');
+    await page.locator('#kollisjonMotpartRegnr').press('Tab');
+    
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+
+    /** ----------------------------  */
+    const headerSkader = page.locator('h2', { hasText: 'Skader' });
+    await expect(headerSkader).toBeVisible();
+
+    await page.locator('#skader').click();
+    await page.locator('#skader').fill('Diger bulk på venstre forskjerm');
+    await page.locator('#skader').press('Tab');
+    await page.getByText('Har du bestilt time på verksted?');
+    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').first().click();
+    await page.locator('#verksted').click();
+    await page.locator('#verksted').fill('Kvikk Fiks AS');
+    await page.locator('#verksted').press('Tab');
+
+    await page.getByText('Last opp skademelding og bilder');
+    await page.getByRole('button', { name: 'Velg filer' }).click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    const fileChooser = await fileChooserPromise;
+
+
+    await page.getByRole('button', { name: 'Send inn' }).click();
+})
+
+test('Skademeldingskjema etter påkjøring av dyr', async ({ page }) => {
+
+    await page.goto('https://nettskjema.test.fremtind.no/skjema/katalog/skademelding-autolease');
+
+    await expect(page).toHaveTitle('Skademelding Autolease');
+
+    const headerInformasjon = page.locator('h2', { hasText: 'Informasjon' });
+    await expect(headerInformasjon).toBeVisible();
+
+    await page.locator('#innmelderFnr').click();
+    await page.locator('#innmelderFnr').fill('09058435551');
+    await page.locator('#innmelderFnr').press('Tab');
+    await page.locator('#innmelderNavn').click();
+    await page.locator('#innmelderNavn').fill('Anders And');
+    await page.locator('#innmelderNavn').press('Tab');
+    await page.locator('#bedriftNavn').click();
+    await page.locator('#bedriftNavn').fill('Selskapet AS');
+    await page.locator('#bedriftNavn').press('Tab');
+    await page.locator('#bedriftOrgnr').click();
+    await page.locator('#bedriftOrgnr').fill('733801433');
+    await page.locator('#bedriftOrgnr').press('Tab');
+    await page.locator('#regNr').click();
+    await page.locator('#regNr').fill('AB12345');
+    await page.locator('#regNr').press('Tab');
+    await page.locator('#kontaktEpost').click();
+    await page.locator('#kontaktEpost').fill('ole@olsen.no');
+    await page.locator('#kontaktEpost').press('Tab');
+    await page.locator('#kontaktTelefon').click();
+    await page.locator('#kontaktTelefon').fill('22334455');
+    await page.locator('#kontaktTelefon').press('Tab');
+    /** */
+    await page.locator('#skadeDato');
+    await page.getByTestId('jkl-icon-button').click();
+    await page.getByLabel('Velg år').selectOption('2024');
+    await page.getByLabel('Velg måned').selectOption('7');
+    await page.getByLabel('22. august').click();
+
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+    
+    /** ----------------------------  */
+    const headerHvaHarSkjedd = page.locator('h2', { hasText: 'Hva har skjedd?' });
+    await expect(headerHvaHarSkjedd).toBeVisible();
+    
+    await page.locator('#aarsak').click();
+    /**
+    await page.locator('label').filter({ hasText: 'Kollisjon med annet kjøretøy' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Påkjøring av dyr' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Tyveri/innbrudd' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Andre skader kjøretøy' }).locator('span').first().click();
+     */
+    await page.locator('label').filter({ hasText: 'Påkjøring av dyr' }).locator('span').first().click();
+
+    const headerPaakjoeringAvDyr = page.locator('h2', { hasText: 'Påkjøring av dyr' });
+    await expect(headerPaakjoeringAvDyr).toBeVisible();
+
+    await page.locator('#dyrHvor').click();
+    await page.locator('#dyrHvor').fill('På veien til byen');
+    await page.locator('#dyrHvor').press('Tab');
+    await page.locator('#dyrSjaforNavn').click();
+    await page.locator('#dyrSjaforNavn').fill('Pelle Pettersen');
+    await page.locator('#dyrSjaforNavn').press('Tab');
+    await page.locator('#dyrSjaforAlder').click();
+    await page.locator('#dyrSjaforAlder').fill('33');
+    await page.locator('#dyrSjaforAlder').press('Tab');
+    await page.locator('#dyrHvordan').click();
+    await page.locator('#dyrHvordan').fill('Plutselig sto det en bil i veikanten');
+    await page.locator('#dyrHvordan').press('Tab');
+    await page.locator('#dyrDekkType').click();
+    await page.locator('#dyrDekkType').filter({ hasText: 'Sommerdekk' }).locator('span').first().click();
+    await page.locator('#dyrDekkType').press('Tab');
+    await page.locator('#dyrMeldtFra').click();
+    await page.locator('#dyrMeldtFra').filter({ hasText: 'Nei' }).locator('span').first().click();
+    await page.locator('#dyrMeldtFra').press('Tab');
+
+    /** ----------------------------  */
+    const headerSkader = page.locator('h2', { hasText: 'Skader' });
+    await expect(headerSkader).toBeVisible();
+
+    await page.locator('#skader').click();
+    await page.locator('#skader').fill('Diger bulk på venstre forskjerm');
+    await page.locator('#skader').press('Tab');
+    await page.locator('#verkstedTime').click();
+    await page.locator('#verkstedTime').fill('1');
+    await page.locator('#verkstedTime').press('Tab');
+    await page.locator('#verksted').click();
+    await page.locator('#verksted').fill('Kvikk Fiks AS');
+    await page.locator('#verksted').press('Tab');
+    await page.locator('#skademelding').click();
+    
+    await page.getByText('Last opp skademelding og bilder');
+    await page.getByRole('button', { name: 'Velg filer' }).click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    const fileChooser = await fileChooserPromise;
+})
+
+
+test('Skademeldingskjema etter tyveri/innbrudd', async ({ page }) => {
+
+    await page.goto('https://nettskjema.test.fremtind.no/skjema/katalog/skademelding-autolease');
+
+    await expect(page).toHaveTitle('Skademelding Autolease');
+
+    const headerInformasjon = page.locator('h2', { hasText: 'Informasjon' });
+    await expect(headerInformasjon).toBeVisible();
+
+    await page.locator('#innmelderFnr').click();
+    await page.locator('#innmelderFnr').fill('09058435551');
+    await page.locator('#innmelderFnr').press('Tab');
+    await page.locator('#innmelderNavn').click();
+    await page.locator('#innmelderNavn').fill('Anders And');
+    await page.locator('#innmelderNavn').press('Tab');
+    await page.locator('#bedriftNavn').click();
+    await page.locator('#bedriftNavn').fill('Selskapet AS');
+    await page.locator('#bedriftNavn').press('Tab');
+    await page.locator('#bedriftOrgnr').click();
+    await page.locator('#bedriftOrgnr').fill('733801433');
+    await page.locator('#bedriftOrgnr').press('Tab');
+    await page.locator('#regNr').click();
+    await page.locator('#regNr').fill('AB12345');
+    await page.locator('#regNr').press('Tab');
+    await page.locator('#kontaktEpost').click();
+    await page.locator('#kontaktEpost').fill('ole@olsen.no');
+    await page.locator('#kontaktEpost').press('Tab');
+    await page.locator('#kontaktTelefon').click();
+    await page.locator('#kontaktTelefon').fill('22334455');
+    await page.locator('#kontaktTelefon').press('Tab');
+    /** */
+    await page.locator('#skadeDato');
+    await page.getByTestId('jkl-icon-button').click();
+    await page.getByLabel('Velg år').selectOption('2024');
+    await page.getByLabel('Velg måned').selectOption('7');
+    await page.getByLabel('22. august').click();
+
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+
+    
+    /** ----------------------------  */
+    const headerHvaHarSkjedd = page.locator('h2', { hasText: 'Hva har skjedd?' });
+    await expect(headerHvaHarSkjedd).toBeVisible();
+    
+    await page.locator('#aarsak').click();
+    /**
+    await page.locator('label').filter({ hasText: 'Kollisjon med annet kjøretøy' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Påkjøring av dyr' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Tyveri/innbrudd' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Andre skader kjøretøy' }).locator('span').first().click();
+     */
+    await page.locator('label').filter({ hasText: 'Tyveri/innbrudd' }).locator('span').first().click();
+
+    /** ----------------------------  */
+    const headerTyveriInnbrudd = page.locator('h2', { hasText: 'Tyveri/innbrudd' });
+    await expect(headerTyveriInnbrudd).toBeVisible();
+
+    await page.locator('#tyveriParkert').click();
+    await page.locator('#tyveriParkert').fill('Utenfor butikken');
+    await page.locator('#tyveriParkert').press('Tab');
+    await page.locator('#tyveriHvaHarSkjedd').click();
+    await page.locator('#tyveriHvaHarSkjedd').fill('Noen har knust sidevinduet og fått åpnet døren');
+    await page.locator('#tyveriHvaHarSkjedd').press('Tab');
+    await page.locator('#tyveriSkader').click();
+    await page.locator('#tyveriSkader').fill('Bilen er totalt ramponert');
+    await page.locator('#tyveriSkader').press('Tab');
+    await page.locator('#tyveriMeldtPoliti').click();
+    await page.locator('#tyveriMeldtPoliti').fill('Ja');
+    await page.locator('#tyveriMeldtPoliti').press('Tab');
+
+    /** ----------------------------  */
+    const headerSkader = page.locator('h2', { hasText: 'Skader' });
+    await expect(headerSkader).toBeVisible();
+
+    await page.locator('#skader').click();
+    await page.locator('#skader').fill('Diger bulk på venstre forskjerm');
+    await page.locator('#skader').press('Tab');
+    await page.locator('#verkstedTime').click();
+    await page.locator('#verkstedTime').fill('1');
+    await page.locator('#verkstedTime').press('Tab');
+    await page.locator('#verksted').click();
+    await page.locator('#verksted').fill('Kvikk Fiks AS');
+    await page.locator('#verksted').press('Tab');
+    await page.locator('#skademelding').click();
+
+    await page.getByText('Last opp skademelding og bilder');
+    await page.getByRole('button', { name: 'Velg filer' }).click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    const fileChooser = await fileChooserPromise;
+
+})
+
+
+test('Skademeldingskjema etter andre skader kjøretøy', async ({ page }) => {
+
+    await page.goto('https://nettskjema.test.fremtind.no/skjema/katalog/skademelding-autolease');
+
+    await expect(page).toHaveTitle('Skademelding Autolease');
+    
+    const headerInformasjon = page.locator('h2', { hasText: 'Informasjon' });
+    await expect(headerInformasjon).toBeVisible();
+
+    await page.locator('#innmelderFnr').click();
+    await page.locator('#innmelderFnr').fill('09058435551');
+    await page.locator('#innmelderFnr').press('Tab');
+    await page.locator('#innmelderNavn').click();
+    await page.locator('#innmelderNavn').fill('Anders And');
+    await page.locator('#innmelderNavn').press('Tab');
+    await page.locator('#bedriftNavn').click();
+    await page.locator('#bedriftNavn').fill('Selskapet AS');
+    await page.locator('#bedriftNavn').press('Tab');
+    await page.locator('#bedriftOrgnr').click();
+    await page.locator('#bedriftOrgnr').fill('733801433');
+    await page.locator('#bedriftOrgnr').press('Tab');
+    await page.locator('#regNr').click();
+    await page.locator('#regNr').fill('AB12345');
+    await page.locator('#regNr').press('Tab');
+    await page.locator('#kontaktEpost').click();
+    await page.locator('#kontaktEpost').fill('ole@olsen.no');
+    await page.locator('#kontaktEpost').press('Tab');
+    await page.locator('#kontaktTelefon').click();
+    await page.locator('#kontaktTelefon').fill('22334455');
+    await page.locator('#kontaktTelefon').press('Tab');
+    /** */
+    await page.locator('#skadeDato');
+    await page.getByTestId('jkl-icon-button').click();
+    await page.getByLabel('Velg år').selectOption('2024');
+    await page.getByLabel('Velg måned').selectOption('7');
+    await page.getByLabel('22. august').click();
+
+    await page.getByRole('button', { name: 'Fortsett' }).click();
+    
+    /** ----------------------------  */
+    const headerHvaHarSkjedd = page.locator('h2', { hasText: 'Hva har skjedd?' });
+    await expect(headerHvaHarSkjedd).toBeVisible();
+    
+    await page.locator('#aarsak').click();
+    /**
+    await page.locator('label').filter({ hasText: 'Kollisjon med annet kjøretøy' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Påkjøring av dyr' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Tyveri/innbrudd' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Andre skader kjøretøy' }).locator('span').first().click();
+     */
+    await page.locator('label').filter({ hasText: 'Andre skader kjøretøy' }).locator('span').first().click();
+
+    await page.locator('#annetHvor').click();
+    await page.locator('#annetHvor').fill('Utenfor sykehuset');
+    await page.locator('#annetHvor').press('Tab');
+    await page.locator('#annetSjaforNavn').click();
+    await page.locator('#annetSjaforNavn').fill('Gudleiv Knotten');
+    await page.locator('#annetSjaforNavn').press('Tab');
+    await page.locator('#annetSjaforAlder').click();
+    await page.locator('#annetSjaforAlder').fill('33');
+    await page.locator('#annetSjaforAlder').press('Tab');
+    await page.locator('#annetHvordan').click();
+    await page.locator('#annetHvordan').fill('En kampestein rullet ned fra fjellsiden og ut i veien og traff bilen');
+    await page.locator('#annetHvordan').press('Tab');
+    await page.locator('#annetDekkType').click();
+    await page.locator('#annetDekkType').filter({ hasText: 'Sommerdekk' }).locator('span').first().click();
+    await page.locator('#annetDekkType').press('Tab');
+
+
+    /** ----------------------------  */
+    const headerSkader = page.locator('h2', { hasText: 'Skader' });
+    await expect(headerSkader).toBeVisible();
+
+    await page.locator('#skader').click();
+    await page.locator('#skader').fill('Diger bulk på venstre forskjerm');
+    await page.locator('#skader').press('Tab');
+    await page.locator('#verkstedTime').click();
+    await page.locator('#verkstedTime').fill('1');
+    await page.locator('#verkstedTime').press('Tab');
+    await page.locator('#verksted').click();
+    await page.locator('#verksted').fill('Kvikk Fiks AS');
+    await page.locator('#verksted').press('Tab');
+    await page.locator('#skademelding').click();
+
+    await page.getByText('Last opp skademelding og bilder');
+    await page.getByRole('button', { name: 'Velg filer' }).click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    const fileChooser = await fileChooserPromise;
+    await page.getByText('Upload file').click();
+
+    /** ----------------------------  */
+
+})
