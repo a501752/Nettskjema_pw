6c986da1b019d12a4bc1f83c4dc2c4a32d474cb0
Espen Smith <espen.smith@fremtind.no>
2025-04-08 09:54:27 +0200

    NSK-818 Playwright test av klageskjema påbegynt

---
+++ tests/klageskjema.spec.ts
@@ -0,0 +1,70 @@
+import { test, expect } from '@playwright/test';
+
+import * as path from "node:path";
+import { fileURLToPath as nodeFileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+test('Klageskjema privat', async ({ page }) => {
+  
+    await page.goto('https://nettskjema.test.fremtind.no/skjema/katalog/klageskjema');
+
+    await page.getByRole('button', { name: 'Start utfylling' }).click();
+    await page.locator('iframe[title="Innlogging"]').contentFrame().getByRole('textbox', { name: 'Fødselsnummer' }).fill('25878899302');
+    await page.locator('iframe[title="Innlogging"]').contentFrame().getByRole('button', { name: 'Neste' }).click();
+    await page.locator('iframe[title="BankID"]').contentFrame().getByRole('textbox', { name: 'Engangskode' }).click();
+    await page.locator('iframe[title="BankID"]').contentFrame().getByRole('textbox', { name: 'Engangskode' }).fill('otp');
+    await page.locator('iframe[title="BankID"]').contentFrame().getByRole('button', { name: 'Neste' }).click();
+    await page.locator('iframe[title="BankID"]').contentFrame().getByRole('textbox', { name: 'Ditt BankID-passord' }).fill('qwer1234');
+    await page.locator('iframe[title="BankID"]').contentFrame().getByRole('button', { name: 'Neste' }).click();
+
+    await expect(page).toHaveTitle('Klageskjema');
+    await expect(page.getByRole('heading', { name: 'Her kan du sende oss en klage' })).toBeVisible();
+
+    await page.getByRole('heading', { name: 'Her kan du sende oss en klage' }).click({
+      button: 'right'
+    });
+
+    await page.locator('#utfyltDato');
+
+    await page.locator('#segment');
+    await page.locator('label').filter({ hasText: 'Privatkunde' }).locator('span').first().click();
+
+    await page.locator('#fornavn').click();
+    await page.locator('#fornavn').fill('Anders');
+    await page.locator('#fornavn').press('Tab');
+    await page.locator('#etternavn').click();
+    await page.locator('#etternavn').fill('Andersen');
+    await page.locator('#etternavn').press('Tab');
+
+    await page.locator('#adresse').click();
+    await page.locator('#adresse').fill('Kugården 123');
+    await page.locator('#postnr').click();
+    await page.locator('#postnr').fill('1337');
+
+    await page.locator('#saksnr').click();
+    await page.locator('#saksnr').fill('987654321');
+    await page.locator('#beskrivelse').click();
+    await page.locator('#beskrivelse').fill('Alt er galt!');
+
+    await page.getByText('Hvordan vil du vi skal kontakte deg?');
+    await page.locator('label').filter({ hasText: 'Brev' }).locator('span').first().click();
+
+    await page.locator('#epost').click();
+    await page.locator('#epost').fill('kry.kunde@fremtind.no');
+    await page.locator('#tlf').click();
+    await page.locator('#tlf').fill('22334455');
+    
+    await expect(page.locator('#root')).toContainText('Last opp vedlegg (dokumentasjon eller lignende)');
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByRole('button', { name: 'Velg filer' }).click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, 'sample.pdf'));
+
+    await page.getByRole('button', { name: 'Send inn skjemaet til Fremtind' }).click();
+})
+
+
+function fileURLToPath(url: string): string {
+    return nodeFileURLToPath(url);
+}
