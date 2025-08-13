bf2899b6d7c8357f9d9a1660dc4c8d02f781b802
Espen Smith <espen.smith@fremtind.no>
2025-08-13 12:33:27 +0200

    NSK-839 Div rettelser

--- tests/bm-yrkesskade-yrkessykdom.spec.ts
+++ tests/bm-yrkesskade-yrkessykdom.spec.ts
@@ -146,7 +146,7 @@ test('Kunde melder yrkesskade', async ({ page }) => {
     await page.locator('label').filter({ hasText: 'På vei til/fra arbeid' }).locator('span').nth(1).click();
 
     await page.getByText('Skyldes skaden en trafikkulykke?');
-    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').first().click();
+    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').nth(2).click();
 
     await page.locator('#regnr').click();
     await page.locator('#regnr').fill('ZY12345');
@@ -154,11 +154,8 @@ test('Kunde melder yrkesskade', async ({ page }) => {
     await page.locator('#forsikringsselskapBilforsikring').click();
     await page.locator('#forsikringsselskapBilforsikring').fill('Fremtind');
 
-    await page.getByText('Har arbeidstaker meldt fra om ulykken til selskapet');
-    await page.locator('label').filter({ hasText: 'Ja'}).locator('span').first().click();
-    /**
-    await page.locator('[id="jkl-input-\\:r6i\\:"] > div > .jkl-radio-button__label > .jkl-radio-button__dot').first().click();
-     */
+    await page.getByText('Har arbeidstaker meldt fra om ulykken til selskapet?');
+    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').nth(3).click();
 
     await page.locator('#hvaGjordeArbeidstakeren').click();
     await page.locator('#hvaGjordeArbeidstakeren').fill('Sikret skadestedet og dirigerte trafikken utenom');
@@ -172,8 +169,8 @@ test('Kunde melder yrkesskade', async ({ page }) => {
     await page.locator('#hvilkenKroppsdel').click();
     await page.locator('#hvilkenKroppsdel').fill('Nesen');
     
-    await page.getByText('Ble personlig verneutstyr brukt? (valgfritt)');
-    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').nth(1).click();
+    await page.getByText('Ble personlig verneutstyr brukt?');
+    await page.locator('label').filter({ hasText: 'Ja' }).locator('span').nth(3).click();
 
     await page.locator('#oppfolgingssporsmaalVerneustyr').click();
     await page.locator('#oppfolgingssporsmaalVerneustyr').fill('Knebeskyttere');
